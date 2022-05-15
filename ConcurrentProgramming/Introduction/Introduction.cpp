f02a8a5f7d195eced66d8f51a02556445814f2e5
Zavodovski <lqhheart@gmail.com>
2022-05-16 00:55:35 +0800

    Concurrent Programming Introduction

--- ConcurrentProgramming/Introduction/Introduction.cpp
+++ ConcurrentProgramming/Introduction/Introduction.cpp
@@ -14,6 +14,8 @@ int main()
     //t1.join();   //main thread waits for t1 to finish
 
     t1.detach();  //t1 will freely on its own -- daewon process
-    
+
+    if(t1.joinable()) t1.join();
+        //Note: a thread can only be joined or detached once otherwise crashed 
     return 0;
 }
